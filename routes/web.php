be8dfad6033456038070706a591e73178b0a258f
NicoHadisurya <114816055+NicoHadisurya@users.noreply.github.com>
2025-06-18 21:48:06 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/set-locale/{locale}', function($locale){
+    if(in_array($locale,['en','id'])){
+        //create session
+        session(['locale' => $locale]);
+    }
+
+    return redirect()->back();
+})->name('set-locale');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
