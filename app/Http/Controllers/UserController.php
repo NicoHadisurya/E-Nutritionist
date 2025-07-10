e2bb025e2a75b709662304dc7caa7b00542279d2
NicoHadisurya <114816055+NicoHadisurya@users.noreply.github.com>
2025-07-10 13:07:30 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,17 @@ class UserController extends Controller
     public function index(){
         return view('profile');
     }
+    
     public function profile(){
         $dataprof = ProfileModel::all();
         return $dataprof;
+    }
+
+    public function food(){
+        return view('food');
+    }
+    
+    public function (exercise){
+        return view('exercise');
     }
 }
