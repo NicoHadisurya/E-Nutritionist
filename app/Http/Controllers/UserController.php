4b66bd6cb6e888ce0a8c557bb9b04f1806efe3a2
NicoHadisurya <114816055+NicoHadisurya@users.noreply.github.com>
2025-07-10 13:00:12 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ class UserController extends Controller
 {
     //
     public function index(){
-        return view('home');
+        return view('profile');
     }
     public function profile(){
         $dataprof = ProfileModel::all();
